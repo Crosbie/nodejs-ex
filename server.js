38b9f4ad7cef88df77b349998f7064ca89a08f75
Eoin Crosbie <ecrosbie@redhat.com>
2019-02-01 13:24:47 +0000

    fix body-parser

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ var express = require('express'),
     app     = express(),
     eps     = require('ejs'),
     morgan  = require('morgan');
+    bodyParser = require('body-parser');
 
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
